e51edd53af8e6b62a705a1ffd666a935b7d781e8
Duncan <fduncan472@gamail.com>
2024-05-23 16:51:03 +0300

    commit

--- src/main/java/com/dunwam/jepschemistpos/Main.java
+++ src/main/java/com/dunwam/jepschemistpos/Main.java
@@ -1,4 +1,16 @@
 package com.dunwam.jepschemistpos;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmLloader= new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        Scene loginScene = new Scene(fxmLloader.load());
+        stage.setScene(loginScene);
+        stage.show();
+    }
 }
--- src/main/java/com/dunwam/jepschemistpos/contollers/LoginController.java
+++ src/main/java/com/dunwam/jepschemistpos/contollers/LoginController.java
@@ -1,4 +1,39 @@
 package com.dunwam.jepschemistpos.contollers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginController {
+
+    @FXML
+    private AnchorPane AnchorPaneImage;
+
+    @FXML
+    private AnchorPane AnchorPaneLeft;
+
+    @FXML
+    private AnchorPane AnchorPaneRight;
+
+    @FXML
+    private Button CancelButton;
+
+    @FXML
+    private TextField EmailTextField;
+
+    @FXML
+    private Hyperlink ForgotPasswordHyperlink;
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private AnchorPane MainPane;
+
+    @FXML
+    private PasswordField PasswordTextField;
+
 }
