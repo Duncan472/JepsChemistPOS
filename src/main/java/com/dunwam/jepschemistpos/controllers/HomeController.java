a5d90891e57ed1499ed36f26d6460d6f38c25823
Duncan472 <fduncan472@gmail.com>
2025-03-13 23:11:54 +0300

    Added the HomeController

--- src/main/java/com/dunwam/jepschemistpos/controllers/HomeController.java
+++ src/main/java/com/dunwam/jepschemistpos/controllers/HomeController.java
@@ -1,4 +1,55 @@
 package com.dunwam.jepschemistpos.contollers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+
 public class HomeController {
+    @FXML
+    private Spinner<?> cashSpinner;
+
+    @FXML
+    private Label currentCashLabel;
+
+    @FXML
+    private Label currentMpesaLabel;
+
+    @FXML
+    private Label currentStockLabel;
+
+    @FXML
+    private TextArea descriptionTextArea;
+
+    @FXML
+    private LineChart<?, ?> lineChart;
+
+    @FXML
+    private Spinner<?> mpesaSpinner;
+
+    @FXML
+    private Label numberOfServicesLabel;
+
+    @FXML
+    private Label servicesTotalCashLabel;
+
+    @FXML
+    private Label servicesTotalMpesa;
+
+    @FXML
+    private Label totalCashLabel;
+
+    @FXML
+    private Label totalMpesaLabel;
+
+    @FXML
+    private Label totalRevenueLabel;
+
+    @FXML
+    private Label totalStockAddedLabel;
+
+    @FXML
+    private Button withdrawButton;
 }
