3942a6af05df2a492f4150ef539d6411e385d0ce
Duncan472 <fduncan472@gmail.com>
2025-03-19 00:31:00 +0300

    Styled the Create Admin UI

--- src/main/java/com/dunwam/jepschemistpos/controllers/SignUpController.java
+++ src/main/java/com/dunwam/jepschemistpos/controllers/SignUpController.java
@@ -1,4 +1,38 @@
 package com.dunwam.jepschemistpos.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpController {
+
+    @FXML
+    private TextField adminEmail;
+
+    @FXML
+    private TextField adminUsername;
+
+    @FXML
+    private Label alertLabel;
+
+    @FXML
+    private Label codeAlert;
+
+    @FXML
+    private PasswordField codeTextField;
+
+    @FXML
+    private PasswordField confirmPassword;
+
+    @FXML
+    private PasswordField enterPassword;
+
+    @FXML
+    private Button getCodeButton;
+
+    @FXML
+    private Button savePassword;
+
 }
