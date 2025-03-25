3df782b1f30cbbca7a09db6144d08d54739e5d52
Duncan472 <fduncan472@gmail.com>
2025-03-26 00:00:08 +0300

    Added the conroller for forgot Password

--- src/main/java/com/dunwam/jepschemistpos/controllers/ForgotPasswordController.java
+++ src/main/java/com/dunwam/jepschemistpos/controllers/ForgotPasswordController.java
@@ -1,4 +1,38 @@
 package com.dunwam.jepschemistpos.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ForgotPasswordController {
+
+    @FXML
+    private TextField adminEmailTextfield;
+
+    @FXML
+    private Label codeAlert;
+
+    @FXML
+    private PasswordField confirmNewPassword;
+
+    @FXML
+    private Label emailAlert;
+
+    @FXML
+    private TextField emailCodeTextfield;
+
+    @FXML
+    private TextField emailToChangePassword;
+
+    @FXML
+    private PasswordField enterNewPassword;
+
+    @FXML
+    private Button getCodeButton;
+
+    @FXML
+    private Button setNewPasswordButton;
+
 }
