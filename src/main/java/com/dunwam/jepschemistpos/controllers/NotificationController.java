4bf94e9a6bd0cf47775c26769660f85f91df8bd9
Duncan472 <fduncan472@gmail.com>
2025-03-30 23:58:07 +0300

    Created and Modified the notification's page

---
+++ src/main/java/com/dunwam/jepschemistpos/controllers/NotificationController.java
@@ -0,0 +1,18 @@
+package com.dunwam.jepschemistpos.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+
+public class NotificationController {
+
+
+        @FXML
+        private TableView<?> expiredGoodsTableView;
+
+        @FXML
+        private TableView<?> outOfStockTableView;
+
+
+
+}
