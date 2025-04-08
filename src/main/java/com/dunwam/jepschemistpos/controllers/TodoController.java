4915055b31e418f82a45804231622a014dd928a2
Duncan472 <fduncan472@gmail.com>
2025-04-08 23:29:01 +0300

    modified the todo controller class

--- src/main/java/com/dunwam/jepschemistpos/controllers/TodoController.java
+++ src/main/java/com/dunwam/jepschemistpos/controllers/TodoController.java
@@ -1,4 +1,24 @@
 package com.dunwam.jepschemistpos.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class TodoController {
+
+
+        @FXML
+        private Button todoCancelButton;
+
+        @FXML
+        private TextArea todoDescription;
+
+        @FXML
+        private Button todoSaveButton;
+
+        @FXML
+        private TextField todoTitle;
+
+    }
+
 }
